e088aa45e78f58f30d61a1a06687cc0a518abc45
KFolwarski20 <konfolw@wp.pl>
2025-11-02 11:47:05 +0100

    Add the cart to the E-commerce app. Creating Cart class, methods, forms, views

---
+++ myshop/cart/cart.py
@@ -0,0 +1,72 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+
+class Cart(object):
+    def __init__(self, request):
+        """
+        Cart initialization.
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # Save the empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add_to_cart(self, product, quantity=1, update_quantity=False):
+        """
+        Add product to cart or change the quantity of the product.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # Sign the session as "modified" to be sure that the session was saved.
+        self.session.modified = True
+
+    def remove_from_cart(self, product):
+        """
+        Remove product from the cart.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Iterate over the elements of the cart and get the elements from database.
+        """
+        product_ids = self.cart.keys()
+        # Get product objects and add it to the cart.
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Calculate the number of items in the cart.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear_cart(self):
+        # Clear the cart in session
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
