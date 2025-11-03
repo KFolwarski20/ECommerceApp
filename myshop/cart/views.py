5585a67d9d3926f6dee5a9a71f5b0d7199436adc
KFolwarski20 <konfolw@wp.pl>
2025-11-03 14:00:17 +0100

    Upgrade the cart funcionality. Implement change quantity of the products, correct delete method. Creating processor for the cart.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -12,7 +12,7 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add_to_cart(product=product, quantity=cd['quantity'], update_quantity=cd.get('update', False))
+        cart.add_to_cart(product=product, quantity=cd['quantity'], update_quantity=cd.get('update', True))
     return redirect('cart:cart_detail')
 
 
@@ -26,4 +26,7 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'override': True})
     return render(request, 'cart/detail.html', {'cart': cart})
