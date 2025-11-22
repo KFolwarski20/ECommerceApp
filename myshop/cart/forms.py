f985276390a6ac9c6a3629ccff11d877ce06043b
KFolwarski20 <konfolw@wp.pl>
2025-11-22 23:22:43 +0100

    Add translation for the templates 'base.html' etc.

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -5,5 +5,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=lambda x: int(x), label=_('Quantity'))
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=lambda x: int(x), label=_('Ilość'))
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
